48b6796ee1820d366fbdf71df729da5e90cbf07d
강한길 <59948163+gksrlf@users.noreply.github.com>
2023-04-03 16:06:51 +0900

    setting complete

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">main</div>;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './main';
 
 const container = document.getElementById('root');
 const root = createRoot(container as Element);
